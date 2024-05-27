53bc4e1fece9dc292ce9d8e56de54b0fd21f81f6
Metaniya <125639579+Metaniya@users.noreply.github.com>
2024-05-27 17:45:25 +0300

    Stack.java

--- package2/Stackimplimentation.java
+++ package2/Stackimplimentation.java
@@ -1,45 +1,48 @@
 package package2;
 
-public class Stackimplimentation {int[] stack = new int[3];
-    int top = 0;
+public class Stack {
+    private int[] stack;
+    private int top;
 
-    public class Stack {
-        private int capacity;
-        private int[] stack;
-        private int top;
+    public Stack(int capacity) {
+        stack = new int[capacity];
+        top = -1;
+    }
 
-        public Stack(int capacity) {
-            this.capacity = capacity;
-            this.stack = new int[capacity];
-            this.top = -1;
+    // Push operation
+    public void push(int element) {
+        if (top == stack.length - 1) {
+            System.out.println("Error: Stack is full");
+            return;
         }
+        stack[++top] = element;
+    }
 
-        public void push(int item) {
-            if (isFull()) {
-                throw new RuntimeException("Stack is full");
-            }
-            stack[++top] = item;
+    // Pop operation
+    public int pop() {
+        if (top == -1) {
+            System.out.println("Error: Stack is empty");
+            return -1;
         }
+        return stack[top--];
+    }
 
-        public int pop() {
-            if (isEmpty()) {
-                throw new RuntimeException("Stack is empty");
-            }
-            return stack[top--];
+    // Peek operation (get the top element without removing it)
+    public int peek() {
+        if (top == -1) {
+            System.out.println("Error: Stack is empty");
+            return -1;
         }
+        return stack[top];
+    }
 
-        public int peek() {
-            if (isEmpty()) {
-                throw new RuntimeException("Stack is empty");
-            }
-            return stack[top];
-        }
+    // Check if the stack is empty
+    public boolean isEmpty() {
+        return top == -1;
+    }
 
-        public boolean isEmpty() {
-            return top == -1;
-        }
-
-        public boolean isFull() {
-            return top == capacity - 1;
-        }
-    }}
+    // Get the current size of the stack
+    public int size() {
+        return top + 1;
+    }
+}
